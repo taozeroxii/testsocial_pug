8ad27e040f6a82867114bf03683b5f25ba1b7fe1
NewbieTao <39129447+taozeroxii@users.noreply.github.com>
2023-03-29 19:53:18 +0700

    add midderware forget login

--- routers/auth.js
+++ routers/auth.js
@@ -1,12 +1,17 @@
 const validateRegister = require('../validates/validateRegister')
+const guestOnly = require('../middleware/guestOnly')
 
 const router = require('express').Router()
+router.use(guestOnly)
 
 router.get('/login', require('../controllers/auth/getLogin'))
 router.get('/register', require('../controllers/auth/getRegister'))
 
 router.post('/login', require('../controllers/auth/postLogin'))
-router.post('/register', validateRegister, require('../controllers/auth/postRegister')
+router.post(
+  '/register',
+  validateRegister,
+  require('../controllers/auth/postRegister')
 )
 
 module.exports = router
